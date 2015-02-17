51c931953b712de864ee996015292cd9811fbaea
Kelvin Rodriguez <kelvin060@gmail.com>
2015-02-17 02:28:02 -0500

    Create 1M assoc between comments and user/post

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217071518) do
+ActiveRecord::Schema.define(version: 20150217072030) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: true do |t|
