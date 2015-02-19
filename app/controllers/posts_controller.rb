16bdb1c937d742bd31f8328841908c2a7291def5
Kelvin Rodriguez <kelvin060@gmail.com>
2015-02-19 00:01:40 -0500

    Add edit and create and move post form to partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,11 +23,18 @@ class PostsController < ApplicationController
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params)
+      flash[:notice] = "Your post was updated."
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
   private
