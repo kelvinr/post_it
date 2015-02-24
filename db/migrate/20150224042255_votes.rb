520c0ede475b1636ffa8f9cb89c4e0a26fc8878b
Kelvin Rodriguez <kelvin060@gmail.com>
2015-02-23 23:51:54 -0500

    Create votes polymorphic table and add associations

---
+++ db/migrate/20150224042255_votes.rb
@@ -0,0 +1,11 @@
+class Votes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.boolean :vote
+      t.integer :user_id
+      t.references :voteable, polymorphic: true
+
+      t.timestamps
+    end
+  end
+end
