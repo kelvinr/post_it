21b3d4169291e982ee4405736b36a4e1210cb6a7
Kelvin Rodriguez <kelvin060@gmail.com>
2015-02-26 01:04:41 -0500

    Remove unused method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,6 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def current_vote
-    @current_vote ||= Vote.find_by(user_id: current_user.id)
-  end
-
   def logged_in?
     !!current_user
   end
