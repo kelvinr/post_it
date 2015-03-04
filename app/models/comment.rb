5ad7c8a76382515ba3b6ac987a70e5a81b7eafe1
Kelvin Rodriguez <kelvin060@gmail.com>
2015-03-04 00:45:41 -0500

    Extract voting logic into Voteable module

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,20 +1,8 @@
 class Comment < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
-  has_many :votes, as: :voteable
 
   validates :body, presence: true
-
-  def counter(params)
-    params == 'true' ? self.increment!(:vote_count) : self.decrement!(:vote_count)
-  end
-
-  def check_vote(user)
-    @current_vote ||= self.votes.find_by(creator: user)
-  end
-
-  def change_vote(params)
-    self.counter(params) if @current_vote.vote.to_s != params
-    @current_vote.update(vote: params)
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,28 +1,16 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true, length: {minimum: 5}
   validates :url, presence: true, uniqueness: true
   validates :description, presence: true, length: {minimum: 10}
 
   before_save :generate_slug, on: :create
-
-  def counter(params)
-    params == 'true' ? self.increment!(:vote_count) : self.decrement!(:vote_count)
-  end
-
-  def check_vote(user)
-    @current_vote ||= self.votes.find_by(creator: user)
-  end
-
-  def change_vote(params)
-    self.counter(params) if @current_vote.vote.to_s != params
-    @current_vote.update(vote: params)
-  end
 
   def generate_slug
     the_slug = to_slug(self.title)
